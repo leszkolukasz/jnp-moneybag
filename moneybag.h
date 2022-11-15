5a899c25274b607021bdb5ae802dcd4f7c8cac14
Łukasz Leszko <leszko.lucas@gmail.com>
2022-11-15 22:13:37 +0100

    Move stuff

--- moneybag.h
+++ moneybag.h
@@ -2,6 +2,30 @@
 
 #include <cstdint>
 #include <string>
+#include <stdexcept>
+
+namespace
+{
+    template<class T>
+    constexpr auto overflow_add(T a, T b)
+    {
+        T tmp;
+        if (__builtin_add_overflow(a, b, &tmp))
+            throw std::out_of_range("Addition integer overflow");
+        
+        return tmp;
+    }
+
+    template<class T>
+    constexpr auto overflow_mul(T a, T b)
+    {
+        T tmp;
+        if (__builtin_mul_overflow(a, b, &tmp))
+            throw std::out_of_range("Multiplication integer overflow");
+        
+        return tmp;
+    }
+}
 
 class Moneybag
 {
@@ -10,9 +34,9 @@ public:
 
     Moneybag() = delete;
 
-    coin_number_t livre_number() const;
-    coin_number_t solidus_number() const;
-    coin_number_t denier_number() const;
+    constexpr coin_number_t livre_number() const {return 0;};
+    constexpr coin_number_t solidus_number() const {return 0;};
+    constexpr coin_number_t denier_number() const {return 0;};
 private:
 };
 
@@ -21,15 +45,36 @@ class Value
 public:
     Value() = delete;
     ~Value() = default;
-    explicit Value(const Moneybag::coin_number_t&);
-    explicit Value(const Moneybag&);
-    explicit Value(const Value&) = default;
-    explicit Value(Value&&) = default;
+
+    constexpr explicit Value(const Moneybag::coin_number_t& value)
+        :value(value)
+    {}
+
+    constexpr explicit Value(const Moneybag& moneybag)
+    {
+        auto mul1 = overflow_mul(moneybag.livre_number(), static_cast<Moneybag::coin_number_t>(livre_to_denier));
+        auto mul2 = overflow_mul(moneybag.solidus_number(), static_cast<Moneybag::coin_number_t>(solidus_to_denier));
+        auto add = overflow_add(mul1, mul2);
+        auto res = overflow_add(add, moneybag.denier_number());
+
+        value = res;
+    }
+
+    constexpr explicit Value(const Value&) = default;
+    constexpr explicit Value(Value&&) = default;
 
     Value& operator= (const Value&) = default;
     Value& operator= (Value&&) = default;
 
     auto operator<=>(const Value&) const = default;
+    constexpr auto operator<=>(const Moneybag::coin_number_t& value) const
+    {
+        return this->value <=> value;
+    }
+    constexpr bool operator==(const Moneybag::coin_number_t& value) const
+    {
+        return this->value == value;
+    }
 
     explicit operator std::string() const
     {
