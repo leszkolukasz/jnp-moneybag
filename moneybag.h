7c38e5795132342a68b50114a0ff08b22bce482b
Łukasz Leszko <leszko.lucas@gmail.com>
2022-11-15 19:55:59 +0100

    Add coin type, const objects

--- moneybag.h
+++ moneybag.h
@@ -1,11 +1,23 @@
 #pragma once
 
+#include <cstdint>
+
 class Moneybag
 {
+public:
+    Moneybag() = delete;
 
-}
+    using coin_number_t = uint_least64_t;
+
+private:
+};
 
 class Value
 {
-    
-}
+public:
+private:
+};
+
+//constexpr Moneybag Livre = Moneybag(1, 0, 0);
+//constexpr Moneybag Solidus = Moneybag(0, 1, 0);
+//constexpr Moneybag Denier = Moneybag(0, 0, 1);
